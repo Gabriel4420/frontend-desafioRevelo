8b1a8946f874fc58f15947093d1ce967dd209411
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2021-12-05 15:52:37 -0300

    Alterando Headers do registro

--- pages/register/index.js
+++ pages/register/index.js
@@ -28,8 +28,8 @@ const Register = () => {
         password: saltpassword,
       },{
         headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin' : '*',
+          'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
         },
       },)
 
